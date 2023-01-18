84350e01079263418e04fdbd28d7f54d71d88044
Rafael Bazzanella <rafaelbazza@gmail.com>
2023-01-18 12:22:25 -0300

    javascript da cromatica resolvido, agora fazer html class das notas

--- Escalacromaticabotaobemolesustenido.js
+++ Escalacromaticabotaobemolesustenido.js
@@ -11,71 +11,65 @@ ra =região aguda
 rg= região grave
 */ 
 
-var notas = document.getElementsByClassName("a ")
+var notas = document.getElementsByClassName("a")
 var notassustenido = document.getElementsByClassName('s')
 var notasbemol = document.getElementsByClassName('b')
-
-
-
+var dobemol = document.getElementsByClassName('C b')
+var misustenido = document.getElementsByClassName('E s')
 var mostranotasbemol =document.getElementById("escalacromaticacomacidentesembemol")
+var mostranotassustenido =document.getElementById("escalacromaticacomacidentesemsustenido")
+
 
 mostranotasbemol.addEventListener('click', function(){
 
-    for (i=0; i<notas.length; i++){
+   
 
-      var bug = i
-        notas[i].style.display="block"
-       if ( bug == 12 || bug == 35){notas[bug].style.display="none"}                  
+    for ( i = 0; i<notas.length; i++){
+              notas[i].style.display="block"
+                   
 }
 
-    for (i=0; i<notassustenido.length; i++){
-
-        notassustenido[i].style.display="none"
-    
-
-
+    for (i = 0; i<notassustenido.length; i++){
+       notassustenido[i].style.display="none"
+     }
+     
+     
+     for ( i=0; i<dobemol.length; i++){
+     
+        dobemol[i].style.display="none"
     }
-
+    
+    for (i = 0; i<misustenido.length; i++){
+        misustenido[i].style.display="none"
+      }
 
 
 
 })
-
-
-var mostranotassustenido =document.getElementById("escalacromaticacomacidentesemsustenido")
 
 mostranotassustenido.addEventListener('click', function(){
 
-    for (i=0; i<notas.length; i++){
+    for (i = 0; i<notas.length; i++){
 
-         
-
-      var bug = i
         notas[i].style.display="block"
-       if ( bug == 1 || bug == 34){notas[bug].style.display="none"}                  
+                   
 }
 
-    for (i=0; i<notasbemol.length; i++){
+    for (i = 0; i<notasbemol.length; i++){
 
         notasbemol[i].style.display="none"
     
+}
+for ( i=0; i<dobemol.length; i++){
+     
+    dobemol[i].style.display="none"
+}
 
-
-    }
+for (i = 0; i<misustenido.length; i++){
+    misustenido[i].style.display="none"
+  }
 
 
 
 
 })
-
-
-
-
-
-
-
-
-
-
-
-
